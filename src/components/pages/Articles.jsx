faccb506830ccc72466312c89af08e8c59c1b3b2
Layla Rodas <rodas.layla@gmail.com>
2024-02-13 18:36:48 +0100

    show articles ---> Api Rest

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,61 +1,53 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 export const Articles = () => {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  const getArticles = async () => {
+    const url = 'http://localhost:3900/api/articles';
+    let petition = await fetch(url, {
+      method: 'GET'
+    }
+    );
+
+    let result = await petition.json();
+
+
+    console.log(result);
+
+    if (result.status === "success") {
+      setArticles(result.articles);
+    }
+  }
+
   return (
     <>
-      <article className='articleItem'>
-        <div className='mask'>
-          <img src="/images.jpg" alt="" />
-        </div>
-        <div className='info'>
-          <h3 className='title'>Article Name</h3>
-          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptates blanditiis itaque?</p>
+      {articles.length >= 1 ?
+        articles.map((article) => {
+          return (
+            <article key={article.id} className='articleItem'>
+              <div className='mask'>
+                <img src="/images.jpg" alt="" />
+              </div>
+              <div className='info'>
+                <h3 className='title'>{article.title}</h3>
+                <p className='description'>{article.content}</p>
 
-          <button className='edit'>Edit</button>
-          <button className='delete' >Delete</button>
-        </div>
-      </article>
-
-      
-      <article className='articleItem'>
-        <div className='mask'>
-          <img src="/images.jpg" alt="" />
-        </div>
-        <div className='info'>
-          <h3 className='title'>Article Name</h3>
-          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptates blanditiis itaque?</p>
-
-          <button className='edit'>Edit</button>
-          <button className='delete' >Delete</button>
-        </div>
-      </article>
-
-      <article className='articleItem'>
-        <div className='mask'>
-          <img src="/images.jpg" alt="" />
-        </div>
-        <div className='info'>
-          <h3 className='title'>Article Name</h3>
-          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptates blanditiis itaque?</p>
-
-          <button className='edit'>Edit</button>
-          <button className='delete' >Delete</button>
-        </div>
-      </article>
-      
-      <article className='articleItem'>
-        <div className='mask'>
-          <img src="/images.jpg" alt="" />
-        </div>
-        <div className='info'>
-          <h3 className='title'>Article Name</h3>
-          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptates blanditiis itaque?</p>
-
-          <button className='edit'>Edit</button>
-          <button className='delete' >Delete</button>
-        </div>
-      </article>
-    
+                <button className='edit'>Edit</button>
+                <button className='delete' >Delete</button>
+              </div>
+            </article>
+          )
+        })
+        : <p>There are no articles to show</p>
+      }
 
     </>
   );
