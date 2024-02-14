9b53a3500cae262b18fd65e8a108077e8fc48d44
Layla Rodas <rodas.layla@gmail.com>
2024-02-14 16:38:31 +0100

    form layout

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export const Create = () => {
   return (
-    <div>
-      {/* Your component content here */}
+    <div className='jumbo'>
+      <h1>Create Article</h1>
+      <p>Form</p>
+
+      <form action="" className='form'>
+        <div className='formGroup'>
+          <label htmlFor="title">Title</label>
+          <input type="text" name='title'/>
+        </div>
+        <div className='formGroup'>
+          <label htmlFor="content">Content</label>
+          <textarea type="text" name='content'/>
+        </div>
+        <div className='formGroup'>
+          <label htmlFor="image">Image</label>
+          <input type="file" name="image" id="image" />
+        </div>
+
+        <input type="submit" value="send" />
+        
+      </form>
     </div>
   );
 };
